c1fed27d6b1eea0f2e2dbc4491234007ba0b63b0
meiluzhu <zhumeiluemail@gmail.com>
2023-08-30 11:30:16 +0800

    Add files via upload

---
+++ training_script_Bloodmnist.sh
@@ -0,0 +1,224 @@
+#!/bin/bash
+#SBATCH -J training
+#SBATCH --gres=gpu:1
+#SBATCH --partition=team1
+#SBATCH -w node30
+#SBATCH -c 2
+#SBATCH -N 1
+
+echo "Submitted from:"$SLURM_SUBMIT_DIR" on node:"$SLURM_SUBMIT_HOST
+echo "Running on node "$SLURM_JOB_NODELIST 
+echo "Allocate Gpu Units:"$CUDA_VISIBLE_DEVICES
+
+nvidia-smi
+
+python Fed_unknown_detection_WCE.py \
+--data_root='./datasets/MedMNIST/bloodmnist.npz' \
+--lr=5e-4 \
+--backbone='Resnet18' \
+--dataset='Bloodmnist' \
+--known_class=5 \
+--unknown_class=3 \
+--seed=0 \
+--batchsize=8 \
+--epoches=100 \
+--client_num=16 \
+--worker_steps=1 \
+--mode='DUS_baseline' \
+--dirichlet=0.5 \
+
+python Fed_unknown_detection_WCE.py \
+--data_root='./datasets/MedMNIST/bloodmnist.npz' \
+--lr=5e-4 \
+--backbone='Resnet18' \
+--dataset='Bloodmnist' \
+--known_class=5 \
+--unknown_class=3 \
+--seed=1 \
+--batchsize=8 \
+--epoches=100 \
+--client_num=16 \
+--worker_steps=1 \
+--mode='DUS_baseline' \
+--dirichlet=0.5 \
+
+python Fed_unknown_detection_WCE.py \
+--data_root='./datasets/MedMNIST/bloodmnist.npz' \
+--lr=5e-4 \
+--backbone='Resnet18' \
+--dataset='Bloodmnist' \
+--known_class=5 \
+--unknown_class=3 \
+--seed=2 \
+--batchsize=8 \
+--epoches=100 \
+--client_num=16 \
+--worker_steps=1 \
+--mode='DUS_baseline' \
+--dirichlet=0.5 \
+
+python Fed_unknown_detection_WCE.py \
+--data_root='./datasets/MedMNIST/bloodmnist.npz' \
+--lr=1e-4 \
+--backbone='Resnet18' \
+--dataset='Bloodmnist' \
+--known_class=5 \
+--unknown_class=3 \
+--seed=0 \
+--batchsize=8 \
+--epoches=30 \
+--client_num=16 \
+--worker_steps=1 \
+--mode='DUS_CUS_finetune' \
+--eps=0.1 \
+--num_steps=1 \
+--unknown_weight=1. \
+--dirichlet=0.5 \
+--start_epoch='[5,10,15,20,25]'
+--sample_from=8 \
+
+python Fed_unknown_detection_WCE.py \
+--data_root='./datasets/MedMNIST/bloodmnist.npz' \
+--lr=1e-4 \
+--backbone='Resnet18' \
+--dataset='Bloodmnist' \
+--known_class=5 \
+--unknown_class=3 \
+--seed=1 \
+--batchsize=8 \
+--epoches=30 \
+--client_num=16 \
+--worker_steps=1 \
+--mode='DUS_CUS_finetune' \
+--eps=0.1 \
+--num_steps=1 \
+--unknown_weight=1. \
+--dirichlet=0.5 \
+--start_epoch='[5,10,15,20,25]'
+--sample_from=8 \
+
+python Fed_unknown_detection_WCE.py \
+--data_root='./datasets/MedMNIST/bloodmnist.npz' \
+--lr=1e-4 \
+--backbone='Resnet18' \
+--dataset='Bloodmnist' \
+--known_class=5 \
+--unknown_class=3 \
+--seed=2 \
+--batchsize=8 \
+--epoches=30 \
+--client_num=16 \
+--worker_steps=1 \
+--mode='DUS_CUS_finetune' \
+--eps=0.1 \
+--num_steps=1 \
+--unknown_weight=1. \
+--dirichlet=0.5 \
+--start_epoch='[5,10,15,20,25]'
+--sample_from=8 \
+
+
+python Fed_unknown_detection_WCE.py \
+--data_root='./datasets/MedMNIST/bloodmnist.npz' \
+--lr=5e-4 \
+--backbone='Resnet18' \
+--dataset='Bloodmnist' \
+--known_class=3 \
+--unknown_class=5 \
+--seed=0 \
+--batchsize=8 \
+--epoches=100 \
+--client_num=16 \
+--worker_steps=1 \
+--mode='DUS_baseline' \
+--dirichlet=0.5 \
+
+python Fed_unknown_detection_WCE.py \
+--data_root='./datasets/MedMNIST/bloodmnist.npz' \
+--lr=5e-4 \
+--backbone='Resnet18' \
+--dataset='Bloodmnist' \
+--known_class=3 \
+--unknown_class=5 \
+--seed=1 \
+--batchsize=8 \
+--epoches=100 \
+--client_num=16 \
+--worker_steps=1 \
+--mode='DUS_baseline' \
+--dirichlet=0.5 \
+
+python Fed_unknown_detection_WCE.py \
+--data_root='./datasets/MedMNIST/bloodmnist.npz' \
+--lr=5e-4 \
+--backbone='Resnet18' \
+--dataset='Bloodmnist' \
+--known_class=3 \
+--unknown_class=5 \
+--seed=2 \
+--batchsize=8 \
+--epoches=100 \
+--client_num=16 \
+--worker_steps=1 \
+--mode='DUS_baseline' \
+--dirichlet=0.5 \
+
+python Fed_unknown_detection_WCE.py \
+--data_root='./datasets/MedMNIST/bloodmnist.npz' \
+--lr=1e-4 \
+--backbone='Resnet18' \
+--dataset='Bloodmnist' \
+--known_class=3 \
+--unknown_class=5 \
+--seed=0 \
+--batchsize=8 \
+--epoches=30 \
+--client_num=16 \
+--worker_steps=1 \
+--mode='DUS_CUS_finetune' \
+--eps=0.1 \
+--num_steps=1 \
+--unknown_weight=1. \
+--dirichlet=0.5 \
+--start_epoch='[5,10,15,20,25]'
+--sample_from=8 \
+
+python Fed_unknown_detection_WCE.py \
+--data_root='./datasets/MedMNIST/bloodmnist.npz' \
+--lr=1e-4 \
+--backbone='Resnet18' \
+--dataset='Bloodmnist' \
+--known_class=3 \
+--unknown_class=5 \
+--seed=1 \
+--batchsize=8 \
+--epoches=30 \
+--client_num=16 \
+--worker_steps=1 \
+--mode='DUS_CUS_finetune' \
+--eps=0.1 \
+--num_steps=1 \
+--unknown_weight=1. \
+--dirichlet=0.5 \
+--start_epoch='[5,10,15,20,25]'
+--sample_from=8 \
+
+python Fed_unknown_detection_WCE.py \
+--data_root='./datasets/MedMNIST/bloodmnist.npz' \
+--lr=1e-4 \
+--backbone='Resnet18' \
+--dataset='Bloodmnist' \
+--known_class=3 \
+--unknown_class=5 \
+--seed=2 \
+--batchsize=8 \
+--epoches=30 \
+--client_num=16 \
+--worker_steps=1 \
+--mode='DUS_CUS_finetune' \
+--eps=0.1 \
+--num_steps=1 \
+--unknown_weight=1. \
+--dirichlet=0.5 \
+--start_epoch='[5,10,15,20,25]'
+--sample_from=8 \
---
+++ training_script_Hyperkvasir.sh
@@ -0,0 +1,207 @@
+#!/bin/bash
+#SBATCH -J training
+#SBATCH --gres=gpu:1
+#SBATCH --partition=team1
+#SBATCH -w node30
+#SBATCH -c 2
+#SBATCH -N 1
+
+echo "Submitted from:"$SLURM_SUBMIT_DIR" on node:"$SLURM_SUBMIT_HOST
+echo "Running on node "$SLURM_JOB_NODELIST 
+echo "Allocate Gpu Units:"$CUDA_VISIBLE_DEVICES
+
+nvidia-smi
+
+python Fed_unknown_detection_WCE.py \
+--data_root='./datasets/Hyperkvasir_processed/labeled-images/' \
+--lr=5e-4 \
+--backbone='Resnet18' \
+--dataset='Hyperkvasir' \
+--known_class=6 \
+--unknown_class=3 \
+--seed=0 \
+--batchsize=8 \
+--epoches=200 \
+--client_num=8 \
+--worker_steps=1 \
+--mode='DUS_baseline' \
+
+python Fed_unknown_detection_WCE.py \
+--data_root='./datasets/Hyperkvasir_processed/labeled-images/' \
+--lr=5e-4 \
+--backbone='Resnet18' \
+--dataset='Hyperkvasir' \
+--known_class=6 \
+--unknown_class=3 \
+--seed=1 \
+--batchsize=8 \
+--epoches=200 \
+--client_num=8 \
+--worker_steps=1 \
+--mode='DUS_baseline' \
+
+python Fed_unknown_detection_WCE.py \
+--data_root='./datasets/Hyperkvasir_processed/labeled-images/' \
+--lr=5e-4 \
+--backbone='Resnet18' \
+--dataset='Hyperkvasir' \
+--known_class=6 \
+--unknown_class=3 \
+--seed=2 \
+--batchsize=8 \
+--epoches=200 \
+--client_num=8 \
+--worker_steps=1 \
+--mode='DUS_baseline' \
+
+python Fed_unknown_detection_WCE.py \
+--data_root='./datasets/Hyperkvasir_processed/labeled-images/' \
+--lr=1e-4 \
+--backbone='Resnet18' \
+--dataset='Hyperkvasir' \
+--known_class=6 \
+--unknown_class=3 \
+--seed=0 \
+--batchsize=8 \
+--epoches=30 \
+--client_num=8 \
+--worker_steps=1 \
+--mode='DUS_CUS_finetune' \
+--num_steps=5 \
+--unknown_weight=1. \
+--sample_from=8 \
+--start_epoch='[5,10,15,20,25]'
+
+python Fed_unknown_detection_WCE.py \
+--data_root='./datasets/Hyperkvasir_processed/labeled-images/' \
+--lr=1e-4 \
+--backbone='Resnet18' \
+--dataset='Hyperkvasir' \
+--known_class=6 \
+--unknown_class=3 \
+--seed=1 \
+--batchsize=8 \
+--epoches=30 \
+--client_num=8 \
+--worker_steps=1 \
+--mode='DUS_CUS_finetune' \
+--num_steps=5 \
+--unknown_weight=1. \
+--sample_from=8 \
+--start_epoch='[5,10,15,20,25]'
+
+python Fed_unknown_detection_WCE.py \
+--data_root='./datasets/Hyperkvasir_processed/labeled-images/' \
+--lr=1e-4 \
+--backbone='Resnet18' \
+--dataset='Hyperkvasir' \
+--known_class=6 \
+--unknown_class=3 \
+--seed=2 \
+--batchsize=8 \
+--epoches=30 \
+--client_num=8 \
+--worker_steps=1 \
+--mode='DUS_CUS_finetune' \
+--num_steps=5 \
+--unknown_weight=1. \
+--sample_from=8 \
+--start_epoch='[5,10,15,20,25]'
+
+
+python Fed_unknown_detection_WCE.py \
+--data_root='./datasets/Hyperkvasir_processed/labeled-images/' \
+--lr=5e-4 \
+--backbone='Resnet18' \
+--dataset='Hyperkvasir' \
+--known_class=6 \
+--unknown_class=9 \
+--seed=0 \
+--batchsize=8 \
+--epoches=200 \
+--client_num=8 \
+--worker_steps=1 \
+--mode='DUS_baseline' \
+
+python Fed_unknown_detection_WCE.py \
+--data_root='./datasets/Hyperkvasir_processed/labeled-images/' \
+--lr=5e-4 \
+--backbone='Resnet18' \
+--dataset='Hyperkvasir' \
+--known_class=6 \
+--unknown_class=9 \
+--seed=1 \
+--batchsize=8 \
+--epoches=200 \
+--client_num=8 \
+--worker_steps=1 \
+--mode='DUS_baseline' \
+
+python Fed_unknown_detection_WCE.py \
+--data_root='./datasets/Hyperkvasir_processed/labeled-images/' \
+--lr=5e-4 \
+--backbone='Resnet18' \
+--dataset='Hyperkvasir' \
+--known_class=6 \
+--unknown_class=9 \
+--seed=2 \
+--batchsize=8 \
+--epoches=200 \
+--client_num=8 \
+--worker_steps=1 \
+--mode='DUS_baseline' \
+
+
+python Fed_unknown_detection_WCE.py \
+--data_root='./datasets/Hyperkvasir_processed/labeled-images/' \
+--lr=1e-4 \
+--backbone='Resnet18' \
+--dataset='Hyperkvasir' \
+--known_class=6 \
+--unknown_class=9 \
+--seed=0 \
+--batchsize=8 \
+--epoches=30 \
+--client_num=8 \
+--worker_steps=1 \
+--mode='DUS_CUS_finetune' \
+--num_steps=5 \
+--unknown_weight=1. \
+--sample_from=8 \
+--start_epoch='[5,10,15,20,25]'
+
+python Fed_unknown_detection_WCE.py \
+--data_root='./datasets/Hyperkvasir_processed/labeled-images/' \
+--lr=1e-4 \
+--backbone='Resnet18' \
+--dataset='Hyperkvasir' \
+--known_class=6 \
+--unknown_class=9 \
+--seed=1 \
+--batchsize=8 \
+--epoches=30 \
+--client_num=8 \
+--worker_steps=1 \
+--mode='DUS_CUS_finetune' \
+--num_steps=5 \
+--unknown_weight=1. \
+--sample_from=8 \
+--start_epoch='[5,10,15,20,25]'
+
+python Fed_unknown_detection_WCE.py \
+--data_root='./datasets/Hyperkvasir_processed/labeled-images/' \
+--lr=1e-4 \
+--backbone='Resnet18' \
+--dataset='Hyperkvasir' \
+--known_class=6 \
+--unknown_class=9 \
+--seed=2 \
+--batchsize=8 \
+--epoches=30 \
+--client_num=8 \
+--worker_steps=1 \
+--mode='DUS_CUS_finetune' \
+--num_steps=5 \
+--unknown_weight=1. \
+--sample_from=8 \
+--start_epoch='[5,10,15,20,25]'
